3d3ddbd43cbab5aec7ac9cc967dac3bcb6267d41
remico <remicollab@gmail.com>
2020-10-31 01:44:13 +0200

    [main] path() object checks if path is absolute

--- src/actions_gui/path.py
+++ src/actions_gui/path.py
@@ -9,8 +9,12 @@ __all__ = ['path']
 
 class path(str):
     def __new__(cls, *other):
-        root = Path(__file__).parent.resolve()
-        return str.__new__(cls, root.joinpath(*other))
+        if other and Path(other[0]).is_absolute():
+            fullpath = Path(*other)
+        else:
+            root = Path(__file__).parent.resolve()
+            fullpath = root.joinpath(*other)
+        return str.__new__(cls, fullpath)
 
     def join(self, *other):
         return path(Path(self).joinpath(self, *other))
